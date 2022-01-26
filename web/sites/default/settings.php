6e83dea62baf563a0ed6cf47040307764c6f262c
Lucas D Hedding <lucashedding@1463982.no-reply.drupal.org>
2022-01-26 14:39:45 -0600

    update vendor

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -12,14 +12,14 @@ $settings['config_exclude_modules'] = [
   'devel',
 ];
 
-// Override config sync directory set by Pantheon.
-$settings['config_sync_directory'] = '../config/sync';
-
 // Automatically generated include for settings managed by ddev.
 $ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
 if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
   require $ddev_settings;
 }
+
+// Set config sync directory.
+$settings['config_sync_directory'] = '../config/sync';
 
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
